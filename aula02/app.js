1016e6aa82ddcc3bd9c13080495cb172410b3e1b
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2025-10-29 11:46:23 -0300

    atribuição de responsabilidades e modularização

--- aula02/app.js
+++ aula02/app.js
@@ -1,16 +1,25 @@
 var ingredientes = ['mel','água', 'sal', 'mostarda'];
-var modificado = []
 
-for (let i = 0; i < ingredientes.length; i++){
-   let letraInicial = ingredientes[i].charAt(0).toUpperCase(); //o charAt é um método que pega uma letra numa posição especifica da string e o toUpperCase transforma em caixa alta
-   let restoTexto = ingredientes[i].slice(1);
-   let resultado = letraInicial + restoTexto;
-   
-   modificado[i] = resultado;
+function capitalizar(ingredientes){
+    let modificado = [];
+    
+    for (let i = 0; i < ingredientes.length; i++){
+       let letraInicial = ingredientes[i].charAt(0).toUpperCase(); //o charAt é um método que pega uma letra numa posição especifica da string e o toUpperCase transforma em caixa alta
+       let restoTexto = ingredientes[i].slice(1);
+       let resultado = letraInicial + restoTexto;
+       
+       modificado[i] = resultado;
+    }
 
+    return modificado;
 }
 
-var ordenado = modificado.sort(function(a,b){
-    return a.localeCompare(b); //localeCompare leva em consideração o idioma do seu navegador, nesse caso considerando os caracteres acentuados
-});
-console.log(ordenado);
+function ordenar(ingredientes){
+
+    return ingredientes.sort(function(a,b){
+        return a.localeCompare(b); //localeCompare leva em consideração o idioma do seu navegador, nesse caso considerando os caracteres acentuados
+    });
+}
+
+console.log(capitalizar(ingredientes));
+console.log(ordenar(ingredientes));
