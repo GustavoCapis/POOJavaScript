87575bc776d65210301766302343b3395b8f2fcc
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2025-10-30 17:48:29 -0300

    Exportação e Importação de Objetos Literais

--- aula02/app.js
+++ aula02/app.js
@@ -1,10 +1,18 @@
 import funcoes from "./funcoes.js";
+import ingredientes from "./ingredientes.js";
+//TODO: import saladas from "./saladas.js"
 
-var ingredientes = ["mel", "água", "sal", "mostarda"];
-var resultadoCapitalizado = funcoes.capitalizar(ingredientes); //O ponto permite o acesso às propriedades do objeto
-var resultadoOrdenado = funcoes.ordenar(resultadoCapitalizado);
-var resultadoCaixaAlta = funcoes.caixaAlta(resultadoOrdenado);
+console.log(ingredientes);
+console.log(typeof ingredientes); //typeof verifica o tipo de dado
+console.log(Array.isArray(ingredientes));//Verifica se um dado é um vetor(array) retornando true ou false
 
-console.log(resultadoCapitalizado);
-console.log(resultadoOrdenado);
-console.log(resultadoCaixaAlta);
+
+
+// var ingredientes = ["mel", "água", "sal", "mostarda"];
+// var resultadoCapitalizado = funcoes.capitalizar(ingredientes); //O ponto permite o acesso às propriedades do objeto
+// var resultadoOrdenado = funcoes.ordenar(resultadoCapitalizado);
+// var resultadoCaixaAlta = funcoes.caixaAlta(resultadoOrdenado);
+
+// console.log(resultadoCapitalizado);
+// console.log(resultadoOrdenado);
+// console.log(resultadoCaixaAlta);
